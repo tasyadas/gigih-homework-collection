4841858bc91ec2f9c9ce146d67a86eb90cd20b6c
tasyadas <tasyadwiaskarasiahaan@gmail.com>
2021-07-31 18:56:03 +0700

    week 4

---
+++ week3/session2/spec/models/item_spec.rb
@@ -0,0 +1,22 @@
+require './models/item'
+require './db/test_connector'
+
+describe Item do
+    describe '#create' do
+        context 'when given valid parameter' do
+
+            before(:each) do
+                params = {
+                    'name' => 'Pasta',
+                    'price' => 
+                }
+            end
+
+            it 'should return one item object' do
+                
+            end
+        end
+    end
+end
+
+
