8316520c79eb5d7860587f66cd1588d04b682493
tasyadas <tasyadwiaskarasiahaan@gmail.com>
2021-08-02 13:45:26 +0700

    test update items and item_categories table

--- week4/homework/spec/models/item_spec.rb
+++ week4/homework/spec/models/item_spec.rb
@@ -42,7 +42,7 @@ describe Item do
                 params = {
                     :name       => 'Pasta',
                     :price      => 54000,
-                    :categories => [1, 2]
+                    :categories => ['1', '2']
                 }
 
                 @item = Item.new(params)
@@ -73,6 +73,50 @@ describe Item do
                 end
 
                 result = @item.save
+
+                expect(result).to eq(true)
+            end
+        end
+    end
+
+    describe '#update_single_item' do
+        context 'when given valid parameter' do
+
+            before(:each) do
+                @update_item = Item.find_single_item(48)
+                @update_item.name = 'Capcay'
+                @update_item.categories = ['1', '3']
+            end
+
+            it 'should validate params' do
+                expect(@update_item.valid?).to eq(true)
+            end
+
+            it 'should update item to db' do
+                mock_client = double
+                allow(Mysql2::Client).to receive(:new).and_return(mock_client)
+
+                # update item
+                expect(mock_client).to receive(:query).with(
+                    'UPDATE items' +
+                    'SET' + 
+                        "items.name = '#{@update_item.name}'," +
+                        "price = '#{@update_item.price}'" +
+                    "WHERE id = '#{@update_item.id}'"
+                )
+
+                # delete previous relation
+                expect(mock_client).to receive(:query).with("DELETE FROM item_categories WHERE item_id = #{@update_item.id}")
+
+                # update new relation
+                @update_item.categories.each do |category|
+                    expect(mock_client).to receive(:query).with(
+                    'INSERT INTO item_categories (item_id, category_id)' +
+                    "VALUES ('#{@update_item.id}', '#{category.to_i}')"
+                    )
+                end
+
+                result = @update_item.update_single_item
 
                 expect(result).to eq(true)
             end
